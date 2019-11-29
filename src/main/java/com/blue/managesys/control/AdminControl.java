47b79d3e2cfb76ddc287cf797f210d945a5f46c1
MyPC <1203473489@qq.com>
2019-11-29 10:42:56 +0800

    增加登录，增加日志打印

--- src/main/java/com/blue/managesys/control/AdminControl.java
+++ src/main/java/com/blue/managesys/control/AdminControl.java
@@ -2,6 +2,7 @@ package com.blue.managesys.control;
 
 import com.blue.managesys.entity.AdminUserEntity;
 import com.blue.managesys.mapper.AdminUserMapper;
+import com.blue.managesys.server.AdminServiceImpl;
 import com.blue.managesys.util.SysUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import java.util.Date;
 @RequestMapping("/admin")
 public class AdminControl {
     @Autowired
-    private AdminUserMapper adminUserMapper;
+    private AdminServiceImpl service;
 
     /**
      *测试数据库连接
@@ -33,10 +34,15 @@ public class AdminControl {
         adminUserEntity.setLoginPass("123");
         System.out.print("123");
         if (adminUserEntity!=null){
-            adminUserMapper.insertAdminUser(adminUserEntity);
+            service.registAdmin(adminUserEntity);
             return "success";
         }else{
             return "error";
         }
+    }
+    @RequestMapping(value = "/login", method = RequestMethod.POST)
+    public AdminUserEntity login(AdminUserEntity adminUserEntity) throws Exception {
+        AdminUserEntity ad = service.adminLogin(adminUserEntity);
+        return ad;
     }
 }
